336a1a6228786ab15a7c8fb6f33a75b331152982
@zamkani <zamkani@gmail.com>
2023-11-26 22:43:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kosmea/converter.java
@@ -0,0 +1,14 @@
+package com.example.kosmea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class converter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_converter);
+    }
+}
